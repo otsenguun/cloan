b17af8ef05fd318e9a9d4f136285c376c1d3e561
otsenguun <tsenguunkoko@gmail.com>
2021-12-28 14:15:51 +0800

    change logo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+        <img src="{{url('images/coinlend_logo.png')}}" alt="" style="width:400px;">
         </x-slot>
 
     
